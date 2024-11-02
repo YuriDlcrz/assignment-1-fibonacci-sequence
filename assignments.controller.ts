d81e383400d669860754d503d2860d1025d5bb94
YuriDlcrz <135201573+YuriDlcrz@users.noreply.github.com>
2024-11-02 11:40:13 +0800

    Update assignments.controller.ts

--- assignments.controller.ts
+++ assignments.controller.ts
@@ -2,25 +2,20 @@ import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 
 @Controller('assignments')
 export class AssignmentsController {
-  @Get('fibonacci/:n')
-  getFibonacci(@Param('n', ParseIntPipe) n: number): { sequence: number[] } {
-    const sequence = this.generateFibonacci(n);
-    return { sequence };
+  @Get('prime/:number')
+  checkPrime(@Param('number', ParseIntPipe) number: number): { isPrime: boolean } {
+    const isPrime = this.isPrime(number);
+    return { isPrime };
   }
 
-  private generateFibonacci(n: number): number[] {
-    if (n <= 0) return [];
-    if (n === 1) return [0];
-
-    const sequence = [0, 1];
-    for (let i = 2; i < n; i++) {
-      sequence.push(sequence[i - 1] + sequence[i - 2]);
+  private isPrime(num: number): boolean {
+    if (num <= 1) return false;
+    if (num <= 3) return true;
+    if (num % 2 === 0 || num % 3 === 0) return false;
+    
+    for (let i = 5; i * i <= num; i += 6) {
+      if (num % i === 0 || num % (i + 2) === 0) return false;
     }
-    return sequence;
-  }
-
-  @Get('test')
-  testRoute() {
-    return { message: 'Test endpoint is working' };
+    return true;
   }
 }
